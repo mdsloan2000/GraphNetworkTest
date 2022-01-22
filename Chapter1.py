0f49db2bb46b4b9ffbeb669f6d5aee64c9e4a3f4
Michael Sloan <mdsloan2000@gmail.com>
2022-01-21 21:56:41 -0500

    Initial Commit

---
+++ Chapter1.py
@@ -0,0 +1,49 @@
+
+# Graph Testing
+
+
+
+# Main Imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sbn 
+
+import sqlalchemy
+
+
+# Graph Setup
+
+import networkx as nx 
+G = nx.Graph()
+
+# Functions
+
+
+def main():
+    try:
+        G.add_node('A')
+        G.add_nodes_from(['B', 'C'])
+        G.add_edge('A', 'B')
+        G.add_edges_from([('B', 'C'), ('A', 'C')])
+        plt.figure(figsize=(7.5, 7.5))
+        nx.draw_networkx(G)
+        plt.show()
+        G.add_edges_from([('B', 'D'), ('C', 'E')]) 
+        nx.draw_networkx(G)
+        plt.show()
+    except:
+      print('Something went wrong')
+    finally:
+      print('The try except is finished')
+    pass
+
+
+
+
+
+# Main Execution
+
+if __name__ =="__main__":
+    main()
+    
